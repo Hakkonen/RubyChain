d2f1f664d8f63217849d12494cd1374aee0474ab
Jayden <hello.jayden@protonmail.com>
2021-08-23 17:59:14 +1000

    Updated names

--- Main/utils/endian.rb
+++ Main/utils/endian.rb
@@ -1,15 +1,17 @@
-def to_32bit_bin( num )
-    [num].pack("V")
-end
+module Endian
+    def to_8bit_lit_end_i( num )
+        [num].pack("V")
+    end
 
-def to_lit_end( hex )
-    [hex].pack("H*").reverse # Note: Reverse is only for little-endianness
-end
+    def to_lit_end_hex32( hex )
+        [hex].pack("H*").reverse # Note: Reverse is only for little-endianness
+    end
 
-def from_32bit_bin( bytes )
-    [bytes].unpack("H*")[0] # The [0] unpacks first returned array
-end
+    def from_8bit_lit_end_i( bytes )
+        [bytes].unpack("H*")[0] # The [0] unpacks first returned array
+    end
 
-def from_lit_end( bytes )
-    [bytes].reverse.unpack("H*")[0]
+    def from_hex32_lit_end( bytes )
+        [bytes].reverse.unpack("H*")[0]
+    end
 end
